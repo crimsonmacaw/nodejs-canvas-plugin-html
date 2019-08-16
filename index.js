46008cb67d4554d9f8bcbfa68f0f73489845e1d8
Robert Bruce <robert.bruce@crimsonmacaw.com>
2019-08-16 11:08:25 +0100

    Added missing require on interpreter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import init from './init';
 
 export default function (kibana) {
   return new kibana.Plugin({
-    require: ['canvas'],
+    require: ['interpreter', 'canvas'],
     name: 'canvas-plugin-html',
     uiExports: {
       canvas: ['plugins/canvas-plugin-html']
